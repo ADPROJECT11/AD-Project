4242fc9006980f72175872d2d4caeb74eb51dfb7
Gan Jia Hui <ganhui@graduate.utm.my>
2023-11-05 00:51:08 +0800

    .

--- resources/views/reservation_cus.blade.php
+++ resources/views/reservation_cus.blade.php
@@ -11,11 +11,55 @@
   <div class="content">
     <div class="empty1"></div>
       <div class="box1">
-        <div class="test">
+        <div class="box1top">
           <p id="p3">Currently in line</P>
           <p id="p4">5</P>
         </div>
+        <div class="box1middletop">
+          <p id="p5">Table</p>
+        </div>
+        <div class="box1middlebottom">
+          <p id="p6">Small (1-4)</p>
+          <p id="p7">Large (5-12)</p>
+        </div>
+        <div class="box1bottom">
+          <p id="p8">?</p>
+          <p id="p9">?</p>
+        </div>
       </div>
+      <div class="empty2"></div>
+        <div class="box2">
+          <p id="p10">Personal Information</p>
+          <div class="form">
+            <form>
+              <div><p2>Name:</p2>
+                <div class="name">
+                  <input type="text" required>
+                </div>
+              </div>
+              <div><p2>Mobile Number:</p2>
+                <div class="mobile">
+                  <!--<img src="{{ asset('image/flag.png')}}" width="20px" height="20px">-->
+                  <input type="text" id="phone" placeholder="+60" required>
+                </div>
+              </div>
+              <div class="tableseat">
+                <div><p id="tab">Table:</p>
+                  <div class="table">
+                    <input id="input1" type="text" required>
+                  </div>
+                </div>
+                <div><p id="sea">Seats:</p>
+                  <div class="seats">
+                    <input id="input2" type="text" required>
+                  </div>
+                </div>
+              </div>
+              <div class="submit">
+                <input type="submit" value="Comfirm Booking">
+              </div>
+            </form>
+        </div>
   </div>
 </body>
 </html>
