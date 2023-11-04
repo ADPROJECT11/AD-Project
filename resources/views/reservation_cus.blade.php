81db0242d48b5cc625291e323ed74508561111d7
Gan Jia Hui <ganhui@graduate.utm.my>
2023-11-04 18:34:26 +0800

    semi complete

--- resources/views/reservation_cus.blade.php
+++ resources/views/reservation_cus.blade.php
@@ -9,7 +9,13 @@
     <p id="p2">Molek Cafe</p>
   </div>
   <div class="content">
-    HIIIII
+    <div class="empty1"></div>
+      <div class="box1">
+        <div class="test">
+          <p id="p3">Currently in line</P>
+          <p id="p4">5</P>
+        </div>
+      </div>
   </div>
 </body>
 </html>
