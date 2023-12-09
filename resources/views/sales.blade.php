a42022a56f442a9e1ef71fe8562822d16198bd22
DirtBean <tanyuan@graduate.utm.my>
2023-12-09 21:08:46 +0800

    Update sales page

--- resources/views/sales.blade.php
+++ resources/views/sales.blade.php
@@ -20,15 +20,26 @@
                 left: 0;
             }
         }
+
+        @media print {
+            body {
+                print-color-adjust: exact;
+            }
+        }
     </style>
+    <script>
+        function printPDF() {
+            print();
+        }
+    </script>
     @vite(['resources/css/app.css','resources/js/app.js'])
     <!-- Load icon library -->
     <title>Sales</title>
 </head>
 
-<body class="bg-gray-200">
+<body class="bg-gray-200 print:bg-white">
     <div class="flex flex-row">
-        <div class="flex flex-row justify-start w-[25%]">
+        <div class="flex flex-row justify-start w-[25%] print:hidden">
             <div class="w-[2%] h-full fixed bg-gradient-to-b from-pigment-indigo-500 to-pigment-indigo-400" id="purplearea"></div>
             <div class="flex flex-col w-[18%] ml-[2%] h-full fixed bg-white">
                 <div class="flex justify-center items-center font-script text-[40px] text-pigment-indigo-500 text-center h-1/4" id="molekcafe">Molek</br>Cafe</div>
@@ -42,18 +53,18 @@
         </div>
 
         <div class="flex flex-col w-full h-full">
-            <header class="fixed w-full px-6 py-4 shadow-md bg-gradient-to-r from-pigment-indigo-500 to-pigment-indigo-400">
+            <header class="fixed w-full px-6 py-4 shadow-md bg-gradient-to-r from-pigment-indigo-500 to-pigment-indigo-400 print:hidden">
                 <div class="return">
                     <a href=/cusmenu><img src="{{ asset('image/return.png')}}" width="30px" height="30px"></a>
                 </div>
             </header>
 
-            <main class="flex px-6 pt-24 pb-6" id="sales">
-                <div class="flex flex-col px-6 py-6 bg-white shadow-md w-full rounded-xl">
+            <main class="flex px-6 pt-24 pb-6 print:pt-0 print:pb-0" id="sales">
+                <div class="flex flex-col px-6 py-6 bg-white shadow-md w-full rounded-xl print:shadow-none">
                     <h1 class="px-4 text-3xl font-bold">Sales</h1>
                     <div class="flex flex-col md:flex-row items-center justify-between space-y-3 md:space-y-0 md:space-x-4 p-4">
                         <div class="w-full md:w-1/2">
-                            <foRM class="flex items-center">
+                            <form class="flex items-center">
                                 <label for="simple-search" class="sr-only">Search</label>
                                 <div class="relative w-full">
                                     <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
@@ -63,11 +74,11 @@
                                     </div>
                                     <input type="text" id="simple-search" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full pl-10 p-2" placeholder="Search" required="">
                                 </div>
-                            </foRM>
+                            </form>
                         </div>
                         <div class="w-full md:w-auto flex flex-col md:flex-row space-y-2 md:space-y-0 items-stretch md:items-center justify-end md:space-x-3 flex-shrink-0">
                             <div class="flex items-center space-x-3 w-full md:w-auto">
-                                <button class="w-full md:w-auto flex items-center justify-center py-2 px-4 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200" type="button">
+                                <button id="generatePDF" class="w-full md:w-auto flex items-center justify-center py-2 px-4 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200" type="button" onclick="printPDF()">
                                     Generate PDF
                                 </button>
                                 <button id="filterDropdownButton" data-dropdown-toggle="filterDropdown" class="w-full md:w-auto flex items-center justify-center py-2 px-4 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200" type="button">
@@ -109,72 +120,83 @@
                     </div>
                     <div class="overflow-x-auto px-4">
                         <table class="w-full text-sm text-left text-gray-500">
-                            <thead class="text-xs text-gray-700 uppercase rounded-t-xl bg-grey-300">
+                            <thead class="text-xs text-gray-700 uppercase rounded-t-xl bg-gray-300">
                                 <tr>
-                                    <th scope="col" class="px-4 py-3 rounded-tl-xl">Product name</th>
+                                    <th scope="col" class="px-4 py-3 rounded-tl-xl">Product ID</th>
+                                    <th scope="col" class="px-4 py-3">Product Name</th>
                                     <th scope="col" class="px-4 py-3">Category</th>
                                     <th scope="col" class="px-4 py-3">Quantity</th>
                                     <th scope="col" class="px-4 py-3 rounded-tr-xl">Value</th>
                                 </tr>
                             </thead>
-                            <tbody class="bg-grey-100">
-                                <tr class="border-b border-grey-300">
-                                    <th scope="row" class="px-4 py-3 font-medium text-gray-900 whitespace-nowrap">Apple iMac 27&#34;</th>
-                                    <td class="px-4 py-3">PC</td>
+                            <tbody class="bg-gray-50">
+                                <tr class="border-b border-gray-300">
+                                    <th scope="row" class="px-4 py-3 font-medium text-gray-900 whitespace-nowrap">F01</th>
+                                    <td class="px-4 py-3">Food 1</td>
+                                    <td class="px-4 py-3">Food</td>
                                     <td class="px-4 py-3">300</td>
-                                    <td class="px-4 py-3">RM 2999</td>
+                                    <td class="px-4 py-3">RM 890</td>
                                 </tr>
-                                <tr class="border-b border-grey-300">
-                                    <th scope="row" class="px-4 py-3 font-medium text-gray-900 whitespace-nowrap">Apple iMac 20&#34;</th>
-                                    <td class="px-4 py-3">PC</td>
+                                <tr class="border-b border-gray-300">
+                                    <th scope="row" class="px-4 py-3 font-medium text-gray-900 whitespace-nowrap">F02</th>
+                                    <td class="px-4 py-3">Food 2</td>
+                                    <td class="px-4 py-3">Food</td>
                                     <td class="px-4 py-3">200</td>
-                                    <td class="px-4 py-3">RM 1499</td>
+                                    <td class="px-4 py-3">RM 1099</td>
                                 </tr>
-                                <tr class="border-b border-grey-300">
-                                    <th scope="row" class="px-4 py-3 font-medium text-gray-900 whitespace-nowrap">Apple iPhone 14</th>
-                                    <td class="px-4 py-3">Phone</td>
-                                    <td class="px-4 py-3">1237</td>
+                                <tr class="border-b border-gray-300">
+                                    <th scope="row" class="px-4 py-3 font-medium text-gray-900 whitespace-nowrap">F03</th>
+                                    <td class="px-4 py-3">Food 3</td>
+                                    <td class="px-4 py-3">Food</td>
+                                    <td class="px-4 py-3">123</td>
                                     <td class="px-4 py-3">RM 999</td>
                                 </tr>
-                                <tr class="border-b border-grey-300">
-                                    <th scope="row" class="px-4 py-3 font-medium text-gray-900 whitespace-nowrap">Apple iPad Air</th>
-                                    <td class="px-4 py-3">Tablet</td>
-                                    <td class="px-4 py-3">4578</td>
-                                    <td class="px-4 py-3">RM 1199</td>
+                                <tr class="border-b border-gray-300">
+                                    <th scope="row" class="px-4 py-3 font-medium text-gray-900 whitespace-nowrap">F04</th>
+                                    <td class="px-4 py-3">Food 4</td>
+                                    <td class="px-4 py-3">Food</td>
+                                    <td class="px-4 py-3">457</td>
+                                    <td class="px-4 py-3">RM 1109</td>
                                 </tr>
-                                <tr class="border-b border-grey-300">
-                                    <th scope="row" class="px-4 py-3 font-medium text-gray-900 whitespace-nowrap">Xbox Series S</th>
-                                    <td class="px-4 py-3">Gaming/Console</td>
+                                <tr class="border-b border-gray-300">
+                                    <th scope="row" class="px-4 py-3 font-medium text-gray-900 whitespace-nowrap">F05</th>
+                                    <td class="px-4 py-3">Food 5</td>
+                                    <td class="px-4 py-3">Food</td>
                                     <td class="px-4 py-3">56</td>
                                     <td class="px-4 py-3">RM 299</td>
                                 </tr>
-                                <tr class="border-b border-grey-300">
-                                    <th scope="row" class="px-4 py-3 font-medium text-gray-900 whitespace-nowrap">PlayStation 5</th>
-                                    <td class="px-4 py-3">Gaming/Console</td>
+                                <tr class="border-b border-gray-300">
+                                    <th scope="row" class="px-4 py-3 font-medium text-gray-900 whitespace-nowrap">F06</th>
+                                    <td class="px-4 py-3">Food 6</td>
+                                    <td class="px-4 py-3">Food</td>
                                     <td class="px-4 py-3">78</td>
                                     <td class="px-4 py-3">RM 799</td>
                                 </tr>
-                                <tr class="border-b border-grey-300">
-                                    <th scope="row" class="px-4 py-3 font-medium text-gray-900 whitespace-nowrap">Xbox Series X</th>
-                                    <td class="px-4 py-3">Gaming/Console</td>
+                                <tr class="border-b border-gray-300">
+                                    <th scope="row" class="px-4 py-3 font-medium text-gray-900 whitespace-nowrap">F07</th>
+                                    <td class="px-4 py-3">Food 7</td>
+                                    <td class="px-4 py-3">Food</td>
                                     <td class="px-4 py-3">200</td>
                                     <td class="px-4 py-3">RM 699</td>
                                 </tr>
-                                <tr class="border-b border-grey-300">
-                                    <th scope="row" class="px-4 py-3 font-medium text-gray-900 whitespace-nowrap">Apple Watch SE</th>
-                                    <td class="px-4 py-3">Watch</td>
-                                    <td class="px-4 py-3">657</td>
+                                <tr class="border-b border-gray-300">
+                                    <th scope="row" class="px-4 py-3 font-medium text-gray-900 whitespace-nowrap">B01</th>
+                                    <td class="px-4 py-3">Beverage 1</td>
+                                    <td class="px-4 py-3">Beverage</td>
+                                    <td class="px-4 py-3">550</td>
                                     <td class="px-4 py-3">RM 399</td>
                                 </tr>
-                                <tr class="border-b border-grey-300">
-                                    <th scope="row" class="px-4 py-3 font-medium text-gray-900 whitespace-nowrap">NIKON D850</th>
-                                    <td class="px-4 py-3">Photo</td>
-                                    <td class="px-4 py-3">465</td>
-                                    <td class="px-4 py-3">RM 599</td>
+                                <tr class="border-b border-gray-300">
+                                    <th scope="row" class="px-4 py-3 font-medium text-gray-900 whitespace-nowrap">B02</th>
+                                    <td class="px-4 py-3">Beverages 2</td>
+                                    <td class="px-4 py-3">Beverage</td>
+                                    <td class="px-4 py-3">665</td>
+                                    <td class="px-4 py-3">RM 699</td>
                                 </tr>
-                                <tr class="border-b border-grey-300">
-                                    <th scope="row" class="px-4 py-3 font-medium text-gray-900 whitespace-nowrap">Monitor BenQ EX2710Q</th>
-                                    <td class="px-4 py-3">TV/Monitor</td>
+                                <tr class="border-b border-gray-300">
+                                    <th scope="row" class="px-4 py-3 font-medium text-gray-900 whitespace-nowrap">O01</th>
+                                    <td class="px-4 py-3">Others 1</td>
+                                    <td class="px-4 py-3">Other</td>
                                     <td class="px-4 py-3">354</td>
                                     <td class="px-4 py-3">RM 499</td>
                                     </td>
@@ -183,11 +205,11 @@
                         </table>
                     </div>
                     <nav class="flex flex-col md:flex-row justify-between items-start md:items-center space-y-3 md:space-y-0 p-4" aria-label="Table navigation">
-                        <span class="text-sm font-noRM al text-gray-500">
+                        <span class="text-sm font-normal text-gray-500">
                             Showing
                             <span class="font-semibold text-gray-900">1-10</span>
                             of
-                            <span class="font-semibold text-gray-900">1000</span>
+                            <span class="font-semibold text-gray-900">50</span>
                         </span>
                         <ul class="inline-flex items-stretch -space-x-px">
                             <li>
@@ -205,13 +227,13 @@
                                 <a href="#" class="flex items-center justify-center text-sm py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700">2</a>
                             </li>
                             <li>
-                                <a href="#" aria-current="page" class="flex items-center justify-center text-sm z-10 py-2 px-3 leading-tight text-primary-600 bg-primary-50 border border-primary-300 hover:bg-primary-100 hover:text-primary-700">3</a>
+                                <a href="#" class="flex items-center justify-center text-sm py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700">3</a>
                             </li>
                             <li>
-                                <a href="#" class="flex items-center justify-center text-sm py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700">...</a>
+                                <a href="#" class="flex items-center justify-center text-sm py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700">4</a>
                             </li>
                             <li>
-                                <a href="#" class="flex items-center justify-center text-sm py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700">100</a>
+                                <a href="#" class="flex items-center justify-center text-sm py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700">5</a>
                             </li>
                             <li>
                                 <a href="#" class="flex items-center justify-center h-full py-1.5 px-3 leading-tight text-gray-500 bg-white rounded-r-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700">
